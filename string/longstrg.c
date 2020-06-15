8040fb478dbdc447d54d2ad0b7955f5266463e2a
CPU_Code <923992029@qq.com>
2020-06-15 20:26:48 +0800

    打印较长的字符串

---
+++ string/longstrg.c
@@ -0,0 +1,20 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-15 20:16:50
+ * @LastEditTime: 2020-06-15 20:23:56
+ * @FilePath: \c_course\string\longstrg.c
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+/* longstrg.c ––打印较长的字符串 */
+#include <stdio.h>
+int main(void)
+{
+    printf("Here's the newest way to print a " "long string.\n"); /* ANSI C */
+
+    return 0;
+}
+
+/*
+Here's the newest way to print a long string.
+*/
