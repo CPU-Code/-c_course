330ea3f81131cd1ff2eab3edd7cc2dd0fa93f3b2
CPU_Code <923992029@qq.com>
2020-06-15 19:09:36 +0800

    错误程序判断故障点

---
+++ first/nogood.c
@@ -0,0 +1,24 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-15 14:20:46
+ * @LastEditTime: 2020-06-15 14:21:43
+ * @FilePath: \c_course\first\nogood.c
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+/* 有错误的程序 */
+#include <stdio.h>
+
+int main(void)
+(
+    int n, int n2, int n3;
+ 
+    /* 该程序有多处错误
+    n = 5;
+    n2 = n * n;
+    n3 = n2 * n2;
+
+    printf("n = %d, n squared = %d, n cubed = %d\n", n, n2, n3)
+    
+    return 0;
+ )
