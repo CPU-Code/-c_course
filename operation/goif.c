c6ac73a925b7d644ad39fe6c6b2a7903d293282c
CPU_Code <923992029@qq.com>
2020-06-16 15:09:33 +0800

    输出并行

--- operation/goif.c
+++ operation/goif.c
@@ -1,7 +1,7 @@
 /*
  * @Author: cpu_code
  * @Date: 2020-06-16 10:58:16
- * @LastEditTime: 2020-06-16 11:00:41
+ * @LastEditTime: 2020-06-16 11:06:10
  * @FilePath: \c_course\operation\goif.c
  * @Gitee: https://gitee.com/cpu_code
  * @CSDN: https://blog.csdn.net/qq_44226094
@@ -12,11 +12,13 @@
 int main(void)
 {
     int jane, tarzan, cheeta;
+
+    //赋值的顺序是从右往左
     cheeta = tarzan = jane = 68;
 
     printf("		   cheeta   tarzan   jane\n");
     printf("First round score %4d %8d %8d\n", cheeta, tarzan, jane);
-    
+
     return 0;
 }
 
